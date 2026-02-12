8cde88708d5eaffa129eb9fe29547d7fd587841c
deven0290 <dmaiers26@bentleyschool.org>
2026-02-12 15:51:04 -0800

    Update CSP_6_01_Library_Basics.py

--- CSP_6_01_Library_Basics.py
+++ CSP_6_01_Library_Basics.py
@@ -1,31 +1,35 @@
-#Please modify the below functions so they fulfill the described process.
-#You must use a function from analytics.py in each question to receive credit.
-#There is no provided test file. You must make and submit one yourself. (check older test files for reference)
+
+import analytics
 
 
-# Modify the below function such that it takes in a list of prices and returns that list with 15% added value
 def process_expenses(rawPrices):
-    pass
+    return [analytics.add_percent(p, 15) for p in rawPrices]
 
 
-# Modify the below function such that it asks the user for n scores and then returns the highest score and the average score of the list.
 def analyze_scores(n):
-    pass
+    scores = []
+    for i in range(n):
+        scores.append(float(input(f"Enter score #{i + 1}: ")))
 
-# Modify the below function such that it takes in a list of strings and returns that list with all spaces removed
-#and all letters lower case.
-def sanitize_usernames():
-   pass
-
-
-# Modify the list such that it takes in a list as an argument and returns a version of the list with all values over 100.
-def identify_outliers():
-    pass
+    highest = analytics.find_max(scores)
+    avg = analytics.average(scores)
+    return highest, avg
 
 
-# Modify the below function such that it takes in a list of items and asks the user for an item to search for.
-#Sanitize the list to only be lower case worsd with no extra spaces
-#Then return the location of the word using binary search if the list is in order and linear search if it is not.
-#example items = ["  Apple", "Banana ", "  CHERRY  ", " date "]
-def search_and_report():
-    pass
+def sanitize_usernames(usernames):
+    return [analytics.to_lower(analytics.remove_spaces(name)) for name in usernames]
+
+
+def identify_outliers(values):
+    return analytics.filter_over(values, 100)
+
+
+def search_and_report(items):
+    cleaned = [analytics.to_lower(analytics.remove_spaces(x)) for x in items]
+
+    target = analytics.to_lower(analytics.remove_spaces(input("Item to search for: ")))
+
+    if analytics.is_sorted(cleaned):
+        return analytics.binary_search(cleaned, target)
+    return analytics.linear_search(cleaned, target)
+
